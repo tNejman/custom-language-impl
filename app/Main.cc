2e4e609723c0e4fd47a14e4d42569c6582ad705d
tnejman <01187260@pw.edu.pl>
2026-04-12 21:31:00 +0200

    Added initial project file structure

---
+++ app/Main.cc
@@ -0,0 +1,5 @@
+#include <iostream>
+
+int main() {
+  std::cout << "This is main.cc file" << std::endl;
+}
---
+++ test/testLexer.cc
@@ -0,0 +1,17 @@
+#include <gtest/gtest.h>
+#include <memory>
+
+class LexerTest : public ::testing::Test {
+ protected:
+
+  void SetUp() override {
+  }
+
+  void TearDown() override {
+
+  }
+};
+
+TEST_F( LexerTest, check_init ) {
+    ASSERT_TRUE(true);
+}
