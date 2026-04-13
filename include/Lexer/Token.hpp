b64170e7a66b66b55eede2eca4162f6de7dc1529
tnejman <01187260@pw.edu.pl>
2026-04-13 14:08:29 +0200

    WIP #2 added initial tests for lexer

--- include/Lexer/Token.hpp
+++ include/Lexer/Token.hpp
@@ -1,15 +1,94 @@
 #pragma once
 
 enum class TokenType {
+    COMMENT,
+    INT_LITERAL,
+    FLOAT_LITERAL,
+    CHAR_LITERAL,
+    BOOL_LITERAL,
 
+    T_INT,
+    T_FLOAT,
+    T_CHAR,
+    T_STR,
+    T_BOOL,
+    T_VOID,
+
+    KW_IF,
+    KW_ELSEIF,
+    KW_ELSE,
+    KW_WHILE,
+    KW_DO,
+    KW_DONE,
+    KW_BREAK,
+    KW_CONTINUE,
+    KW_RET,
+
+    OP_AND,
+    OP_OR,
+    OP_NOT,
+
+    KW_DEF,
+    KW_VAR,
+    KW_COPY,
+    KW_CAST_TO,
+    KW_MUT,
+
+    OP_MAP,
+    OP_EQ,
+    OP_NEQ,
+    OP_GEQ,
+    OP_LEQ,
+
+    OP_ADD_ASSIGN,
+    OP_SUB_ASSIGN,
+    OP_MUL_ASSIGN,
+    OP_DIV_ASSIGN,
+
+    OP_CONAT,
+    OP_DIFF,
+
+    OP_ASSIGN,
+    OP_PLUS,
+    OP_MINUS,
+    OP_MUL,
+    OP_DIV,
+    OP_MOD,
+    OP_LEN,
+    OP_FILTER,
+    OP_GT,
+    OP_LT,
+    OP_REV,
+
+    COLON,
+    COMMA,
+    LPAREN,
+    RPAREN,
+    LBRACKET,
+    RBRACKET,
+
+    IDENTIFIER,
+    NEWLINE,
+    WS,
+    END_OF_FILE
 };
 
 struct Position {
     unsigned int line_;
     unsigned int column_;
+
+    bool operator==(const Position& other) const {
+        return this->line_ == other.line_ && this->column_ == other.column_;
+    }
 };
+
+
 
 struct Token {
     Position position_;
     TokenType type_;
+
+    bool operator==(const Token& other) const {
+        return this->position_ == other.position_ && this->type_ == other.type_;
+    }
 };
--- test/testLexer.cc
+++ test/testLexer.cc
@@ -1,6 +1,9 @@
 #include <gtest/gtest.h>
 
 #include <memory>
+#include <vector>
+
+#include "Lexer/Lexer.h"
 
 class LexerTest : public ::testing::Test {
  protected:
@@ -11,68 +14,388 @@ class LexerTest : public ::testing::Test {
   }
 };
 
+Lexer assertTokensTypeAndPos(const std::string&& code, const std::vector<Token>&& expected) {
+  std::stringstream ss(code);
+  Lexer lexer(ss);
+  for (const auto& exp : expected) {
+    ASSERT_EQ(exp, lexer.getNextToken());
+  }
+  return lexer;
+}
+
+Lexer assertTokensType(const std::string&& code, const std::vector<TokenType>&& expected) {
+  std::stringstream ss(code);
+  Lexer lexer(ss);
+  for (const auto& exp : expected) {
+    ASSERT_EQ(exp, lexer.getNextToken().type_);
+  }
+  return lexer;
+}
+
 TEST_F( LexerTest, check_init ) {
   ASSERT_TRUE( true );
 }
 
-/*
-R"(var counter = 0)"
-R"(float pi = 3.14_15_926)"
-R"(string msg = "Escaped "quotes" and \ slashes")"
-R"(char newline = '\n')"
-R"(bool is_ready = true)"
-R"(def calculate(x: int, y: int) -> int:
-ret x + y
-done)"
-R"(if a >= 10 and b <= 20:
-a += 1
-elseif a != b or not c:
-b -= 1
-else:
-a = 2
-b /= 2
-)"
-R"(while true do
-var arr = [1, 2, 3] ++ [4, 5]
-arr = arr -- [2]
-var size = $arr
-var filtered = arr ?
-var reversed = arr @
-if size > 10 break
-continue
-done)"
-R"(try
-throw exception
-catch
-cast_to int
-must copy
-finally
-ret
-done)"
-R"(# This is a full line comment)"
-R"(var x = 10 # This is an inline comment)"
-R"()"
-R"(    )"
-R"(hidden_var)"
-R"(123_bad_ident)"
-R"(1_000__000)"
-R"(1_000)"
-R"(_1_000)"
-R"(12.34.56)"
-R"(12.34)"
-R"(12.34)"
-R"(.99)"
-R"(99.)"
-R"("Unclosed string)"
-R"("Bad \escape")"
-R"('')"
-R"('ab')"
-R"('\k')"
-R"(!===)"
-R"(===>)"
-R"(->->)"
-R"(+=-==/=)"
-R"(&|^~`)"
-R"(/* block comment? */)"
-R"($?@%+++---)"
-*/
+TEST_F( LexerTest, check_1 ) {
+  assertTokensTypeAndPos("int x = 5", {
+    {TokenType::T_INT, {1, 0}},
+    {TokenType::IDENTIFIER, {1, 5}},
+    {TokenType::OP_ASSIGN, {1, 7}},
+    {TokenType::INT_LITERAl, {1, 9}}
+  });
+}
+
+TEST_F(LexerTest, VariableDeclarations) {
+    assertTokensTypeAndPos("var counter = 0", {
+        {{1, 0}, TokenType::KW_VAR},
+        {{1, 4}, TokenType::IDENTIFIER},
+        {{1, 12}, TokenType::OP_ASSIGN},
+        {{1, 14}, TokenType::INT_LITERAl}
+    });
+
+    assertTokensTypeAndPos("float pi = 3.14_15_926", {
+        {{1, 0}, TokenType::T_FLOAT},
+        {{1, 6}, TokenType::IDENTIFIER},
+        {{1, 9}, TokenType::OP_ASSIGN},
+        {{1, 11}, TokenType::FLOAT_LITERAL}
+    });
+}
+
+TEST_F(LexerTest, Literals) {
+    assertTokensTypeAndPos("char newline = '\\n'", {
+        {{1, 0}, TokenType::T_CHAR},
+        {{1, 5}, TokenType::IDENTIFIER},
+        {{1, 13}, TokenType::OP_ASSIGN},
+        {{1, 15}, TokenType::CHAR_LITERAL},
+        {{1, 21}, TokenType::END_OF_FILE}
+    });
+
+    assertTokensTypeAndPos("bool is_ready = true", {
+        {{1, 0}, TokenType::T_BOOL},
+        {{1, 5}, TokenType::IDENTIFIER},
+        {{1, 14}, TokenType::OP_ASSIGN},
+        {{1, 16}, TokenType::BOOL_LITERAL},
+        {{1, 20}, TokenType::END_OF_FILE}
+    });
+}
+
+TEST_F(LexerTest, FunctionDefinition) {
+    assertTokensType(
+      "def int calculate(int x, int y) do
+          ret x + y
+       done", {
+        TokenType::KW_DEF,
+        TokenType::T_INT,
+        TokenType::IDENTIFIER,
+        TokenType::LPAREN,
+        TokenType::T_INT,
+        TokenType::IDENTIFIER,
+        TokenType::COMMA,
+        TokenType::T_INT,
+        TokenType::IDENTIFIER,
+        TokenType::RPAREN,
+        TokenType::KW_DO,
+        TokenType::NEWLINE,
+
+        TokenType::KW_RET,
+        TokenType::IDENTIFIER,
+        TokenType::OP_PLUS,
+        TokenType::IDENTIFIER,
+        TokenType::NEWLINE,
+
+        TokenType::KW_DONE
+        TokenType::END_OF_FILE
+    });
+}
+
+TEST_F(LexerTest, ControlFlow) {
+    assertTokensType("
+      if (a >= 10 and b <= 20) do
+          a += 1
+      done elseif (a != b or not c) do
+          b -= 1
+      done else do
+          a = 2
+          b /= 2
+      done", {
+        TokenType::KW_IF,
+        TokenType::LPAREN,
+        TokenType::IDENTIFIER,
+        TokenType::OP_GEQ,
+        TokenType::INT_LITERAL,
+        TokenType::OP_AND,
+        TokenType::IDENTIFIER,
+        TokenType::OP_LEQ,
+        TokenType::INT_LITERAL,
+        TokenType::RPAREN,
+        TokenType::KW_DO,
+        TokenType::NEWLINE,
+
+        TokenType::KW_DONE,
+        TokenType::KW_ELSEIF,
+        TokenType::LPAREN,
+        TokenType::IDENTIFIER,
+        TokenType::OP_NEQ,
+        TokenType::IDENTIFIER,
+        TokenType::OP_OR,
+        TokenType::OP_NOT,
+        TokenType::IDENTIFIER,
+        TokenType::RPAREN,
+        TokenType::KW_DO,
+        TokenType::NEWLINE,
+
+        TokenType::IDENTIFIER,
+        TokenType::OP_SUB_ASSIGN,
+        TokenType::INT_LITERAL,
+        TokenType::NEWLINE,
+
+        TokenType::KW_DONE,
+        TokenType::KW_ELSE,
+        TokenType::KW_DO,
+        TokenType::NEWLINE,
+
+        TokenType::IDENTIFIER,
+        TokenType::OP_ASSIGN,
+        TokenType::INT_LITERAL,
+        TokenType::NEWLINE,
+
+        TokenType::IDENTIFIER,
+        TokenType::OP_DIV_ASSIGN,
+        TokenType::INT_LITERAL,
+        TokenType::NEWLINE,
+
+        TokenType::KW_DONE,
+        TokenType::END_OF_FILE
+    });
+}
+
+TEST_F(LexerTest, ConcatenationInitialization) {
+    AssertTokensType("var [int] arr = [1,2,3] ++ [4,5]", {
+        TokenType::KW_VAR,
+        TokenType::LBRACKET,
+        TokenType::T_INT,
+        TokenType::RBRACKET,
+        TokenType::IDENTIFIER,
+        TokenType::OP_ASSIGN,
+        TokenType::LBRACKET,
+        TokenType::INT_LITERAL,
+        TokenType::COMMA,
+        TokenType::INT_LITERAL,
+        TokenType::COMMA,
+        TokenType::INT_LITERAL,
+        TokenType::RBRACKET,
+        TokenType::OP_CONCAT,
+        TokenType::LBRACKET,
+        TokenType::INT_LITERAL,
+        TokenType::COMMA,
+        TokenType::INT_LITERAL,
+        TokenType::RBRACKET,
+        TokenType::END_OF_FILE
+    });
+}
+
+TEST_F(LexerTest, OperatorDiff) {
+    AssertTokensType("arr = arr -- [2]", {
+        TokenType::IDENTIFIER,
+        TokenType::OP_ASSIGN,
+        TokenType::IDENTIFIER,
+        TokenType::OP_DIFF,
+        TokenType::LBRACKET,
+        TokenType::INT_LITERAL,
+        TokenType::RBRACKET,
+    });
+}
+
+TEST_F(LexerTest, OperatorLength) {
+    AssertTokensType("var int size = $arr", {
+        TokenType::KW_VAR,
+        TokenType::T_INT,
+        TokenType::IDENTIFIER,
+        TokenType::OP_ASSIGN,
+        TokenType::OP_LEN,
+        TokenType::IDENTIFIER,
+        TokenType::END_OF_FILE
+    });
+}
+
+TEST_F(LexerTest, OperatorFilter) {
+    AssertTokensType("var [int] filtered = arr ? isBig", {
+        TokenType::KW_VAR,
+        TokenType::LBRACKET,
+        TokenType::T_INT,
+        TokenType::RBRACKET,
+        TokenType::IDENTIFIER,
+        TokenType::OP_ASSIGN,
+        TokenType::IDENTIFIER,
+        TokenType::OP_FILTER,
+        TokenType::IDENTIFIER,
+        TokenType::END_OF_FILE
+    });
+}
+
+TEST_F(LexerTest, OperatorReverse) {
+  AssertTokensType("[int] reversed = @arr", {
+    TokenType::LBRACKET,
+    TokenType::T_INT,
+    TokenType::RBRACKET,
+    TokenType::IDENTIFIER,
+    TokenType::OP_ASSIGN,
+    TokenType::OP_REV,
+    TokenType::IDENTIFIER,
+    TokenType::END_OF_FILE
+  });
+}
+
+TEST_F(LexerTest, WhileBreakContinue) {
+  assertTokensType("
+    while (i > 0) do
+      if (i == 10) do
+        break
+      done elseif (i == 17) do
+        continue
+      done
+    done
+    "), {
+      TokenType::KW_WHILE,
+      TokenType::LPAREN,
+      TokenType::IDENTIFIER,
+      TokenType::OP_GT,
+      TokenType::INT_LITERAL,
+      TokenType::RPAREN,
+      TokenType::KW_DO,
+      TokenType::NEWLINE
+
+      TokenType::KW_IF,
+      TokenType::LPAREN,
+      TokenType::OP_EQ,
+      TokenType::INT_LITERAL,
+      TokenType::RPAREN,
+      TokenType::KW_DO,
+      TokenType::NEWLINE,
+
+      TokenType::KW_BREAK,
+      TokenType::NEWLINE,
+
+      TokenType::KW_DONE,
+      TokenType::KW_ELSEIF,
+      TokenType::LPAREN,
+      TokenType::IDENTIFIER,
+      TokenType::OP_EQ,
+      TokenType::RPAREN,
+      TokenType::KW_DO,
+      TokenType::NEWLINE,
+
+      TokenType::KW_CONTINUE,
+      TokenType::NEWLINE,
+
+      TokenType::KW_DONE,
+      TokenType::NEWLINE,
+
+      TokenType::KW_DONE,
+      TokenType::END_OF_FILE
+    }
+}
+
+TEST_F(LexerTest, FullLineComment) {
+  assertTokensType("
+    # this is a full line comment
+    # int x = 5
+    # previous line was also a comment
+    ", {
+      TokenType::COMMENT,
+      TokenType::NEWLINE,
+      TokenType::COMMENT,
+      TokenType::NEWLINE,
+      TokenType::COMMENT,
+      TokenType::END_OF_FILE
+    });
+}
+
+TEST_F(LexerTest, CommentAfterCode) {
+    AssertTokensType("int x = 10 # comment", {
+        TokenType::T_INT,
+        TokenType::IDENTIFIER,
+        TokenType::OP_ASSIGN,
+        TokenType::INT_LITERAl,
+        TokenType::COMMENT,
+        TokenType::END_OF_FILE
+    });
+}
+
+TEST_F(LexerTest, Whitespaces) {
+    AssertTokensType("            ", {
+        TokenType::END_OF_FILE
+    });
+}
+
+TEST_F(LexerTest, IdentifiersAndNumbers) {
+    AssertTokensType("1_000", {TokenType::INT_LITERAl});
+    AssertTokensType("1____000", {TokenType::INT_LITERAL});
+    AssertTokensType("12.34", {TokenType::FLOAT_LITERAL});
+}
+
+TEST_F(LexerTest, OperatorChains) {
+    AssertTokensType("+=-==/=++", {
+        TokenType::OP_ADD_ASSIGN,
+        TokenType::OP_SUB_ASSIGN,
+        TokenType::OP_ASSIGN,
+        TokenType::OP_DIV_ASSIGN,
+        TokenType::OP_CONCAT
+    });
+}
+
+TEST_F(LexerTest){
+  // vector<int> v{1, 2, 5, 7, 9, 11, 13};
+
+  // x := 5;
+  // l := 0;
+  // r := $(v)-1;
+  // while(l < r){
+  //   mid := (l+r)/2;
+  //   if(v[mid] <= x){
+  //     l = mid;
+  //   }else{
+  //     r = mid-1;
+  //   }
+  // }
+  // cout << r;
+
+  assertTokensType("
+    [int] v = [1, 2, 5]
+    int x = 5
+    var int l = 0
+    var int r = $v - 1
+    while (l < r) do
+      int mid = (l + r) / 2
+      if (v[mid] <= x) do
+        l = mid
+      done else do
+        r = mid - 1
+      done
+    done
+    write(r)
+    write('\n')
+    "), {
+      TokenType::LBRACKET, TokenType::T_INT, TokenType::RBRACKET, TokenType::IDENTIFIER, TokenType::OP_ASSIGN, TokenType::LBRACKET,
+      TokenType::INT_LITERAL, TokenType::COMMA, TokenType::INT_LITERAL, TokenType::COMMA, TokenType::INT_LITERAL,
+      TokenType::RBRACKET, TokenType::NEWLINE,
+
+      TokenType::T_INT, TokenType::IDENTIFIER, TokenType::OP_ASSIGN, TokenType::INT_LITERAL, TokenType::NEWLINE,
+      TokenType::KW_VAR, TokenType::T_INT, TokenType::IDENTIFIER, TokenType::OP_ASSIGN, TokenType::INT_LITERAL, TokenType::NEWLINE,
+      TokenType::KW_VAR, TokenType::T_INT, TokenType::IDENTIFIER, TokenType::OP_ASSIGN,
+          TokenType::OP_LEN, TokenType::IDENTIFIER, TokenType::OP_MINUS, TokenType::INT_LITERAL, TokenType::NEWLINE
+      TokenType::KW_WHILE TokenType::LPAREN, TokenType::IDENTIFIER, TokenType::OP_LTE,
+          TokenType::IDENTIFIER, TokenType::RPAREN, TokenType::KW_DO, TokenType::NEWLINE,
+      TokenType::T_INT, TokenType::IDENTIFIER, TokenType::LPAREN, TokenType::IDENTIFIER, TokenType::OP_PLUS,
+          TokenType::IDENTIFIER, TokenType::RPAREN, TokenType::OP_DIV, TokenType::INT_LITERAL, TokenType::NEWLINE,
+      TokenType::KW_IF, TokenType::LPAREN, TokenType::IDENTIFIER, TokenType::LBRACKET, TokenType::IDENTIFIER, TokenType::RBRACKET,
+          TokenType::OP_LEQ, TokenType::IDENTIFIER, TokenType::RPAREN, TokenType::KW_DO, TokenType::NEWLINE,
+      TokenType::IDENTIFIER, TokenType::OP_ASSIGN, TokenType::IDENTIFIER, TokenType::NEWLINE,
+      TokenType::KW_DONE, TokenType::KW_ELSE, TokenType::KW_DO, TokenType::NEWLINE,
+      TokenType::IDENTIFIER, TokenType::OP_ASSIGN, TokenType::IDENTIFIER, TokenType::OP_MINUS, TokenType::INT_LITERAL, TokenType::NEWLINE,
+      TokenType::KW_DONE, TokenType::NEWLINE,
+      TokenType::KW_DONE, TokenType::NEWLINE,
+      TokenType::IDENTIFIER, TokenType::LPAREN, TokenType::IDENTIFIER, TokenType::RPAREN, TokenType::NEWLINE,
+      TokenType::IDENTIFIER, TokenType::LPAREN, TokenType::CHAR_LITERAL, TokenType::RPAREN, TokenType::END_OF_FILE
+    }
+}
